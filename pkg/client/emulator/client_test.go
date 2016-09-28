47e574b2b09d9a1bb2292f59900299c4d9d30e1d
Jan Chaloupka <jchaloup@redhat.com>
2016-09-28 22:37:56 +0200

    move RESTClient to separate file so it can be used by the client emulator as well

--- pkg/client/emulator/client_test.go
+++ pkg/client/emulator/client_test.go
@@ -6,14 +6,7 @@ import (
 	"k8s.io/kubernetes/pkg/api/unversioned"
 	apitesting "k8s.io/kubernetes/pkg/api/testing"
 	"k8s.io/kubernetes/pkg/api/testapi"
-	"net/http"
-	"k8s.io/kubernetes/pkg/runtime"
-	"bytes"
-	"io/ioutil"
 	"reflect"
-	"k8s.io/kubernetes/pkg/client/restclient"
-	"fmt"
-	"net/url"
 	"strings"
 )
 
@@ -73,109 +66,7 @@ func testReplicationControllersData() *api.ReplicationControllerList {
 	return rc
 }
 
-
-// RESTClient provides a fake RESTClient interface.
-type RESTClient struct {
-	NegotiatedSerializer runtime.NegotiatedSerializer
-
-	Req  *http.Request
-	Resp *http.Response
-	Err  error
-
-	podsDataSource func() *api.PodList
-	servicesDataSource func() *api.ServiceList
-	replicationControllersDataSource func() *api.ReplicationControllerList
-}
-
-func (c *RESTClient) Pods() *api.PodList {
-	return c.podsDataSource()
-}
-
-func (c *RESTClient) Services() *api.ServiceList {
-	return c.servicesDataSource()
-}
-
-func (c *RESTClient) ReplicationControllers() *api.ReplicationControllerList {
-	return c.replicationControllersDataSource()
-}
-
-func (c *RESTClient) Get() *restclient.Request {
-	return c.request("GET")
-}
-
-func (c *RESTClient) Put() *restclient.Request {
-	return c.request("PUT")
-}
-
-func (c *RESTClient) Patch(_ api.PatchType) *restclient.Request {
-	return c.request("PATCH")
-}
-
-func (c *RESTClient) Post() *restclient.Request {
-	return c.request("POST")
-}
-
-func (c *RESTClient) Delete() *restclient.Request {
-	return c.request("DELETE")
-}
-
-func (c *RESTClient) request(verb string) *restclient.Request {
-	config := restclient.ContentConfig{
-		ContentType:          runtime.ContentTypeJSON,
-		GroupVersion:         testapi.Default.GroupVersion(),
-		NegotiatedSerializer: c.NegotiatedSerializer,
-	}
-	ns := c.NegotiatedSerializer
-	serializer, _ := ns.SerializerForMediaType(runtime.ContentTypeJSON, nil)
-	streamingSerializer, _ := ns.StreamingSerializerForMediaType(runtime.ContentTypeJSON, nil)
-	internalVersion := unversioned.GroupVersion{
-		Group:   testapi.Default.GroupVersion().Group,
-		Version: runtime.APIVersionInternal,
-	}
-	internalVersion.Version = runtime.APIVersionInternal
-	serializers := restclient.Serializers{
-		Encoder:             ns.EncoderForVersion(serializer, *testapi.Default.GroupVersion()),
-		Decoder:             ns.DecoderToVersion(serializer, internalVersion),
-		StreamingSerializer: streamingSerializer,
-		Framer:              streamingSerializer.Framer,
-	}
-	return restclient.NewRequest(c, verb, &url.URL{Host: "localhost"}, "", config, serializers, nil, nil)
-}
-
-func (c *RESTClient) Do(req *http.Request) (*http.Response, error) {
-	if c.Err != nil {
-		return nil, c.Err
-	}
-	c.Req = req
-
-	// //localhost/pods?resourceVersion=0
-	parts := splitPath(req.URL.Path)
-	if len(parts) < 1 {
-		return nil, fmt.Errorf("Missing resource in REST client request url")
-	}
-
-	var obj runtime.Object
-
-	switch parts[0] {
-		case "pods":
-			obj = c.Pods()
-		case "services":
-			obj = c.Services()
-		case "replicationcontrollers":
-			obj = c.ReplicationControllers()
-		default:
-			return nil, fmt.Errorf("Resource %s not recognized", parts[0])
-	}
-
-	header := http.Header{}
-	header.Set("Content-Type", runtime.ContentTypeJSON)
-	body := ioutil.NopCloser(bytes.NewReader([]byte(runtime.EncodeOrDie(testapi.Default.Codec(), obj))))
-	c.Resp = &http.Response{StatusCode: 200, Header: header, Body: body}
-
-	return c.Resp, nil
-}
-
-func NewRestClient() *RESTClient {
+func NewTestRestClient() *RESTClient {
 
 	client := &RESTClient{
 		NegotiatedSerializer: testapi.Default.NegotiatedSerializer(),
@@ -226,7 +117,7 @@ func compareItems(expected, actual interface{}) bool {
 
 func TestSyncPods(t *testing.T) {
 
-	fakeClient := NewRestClient()
+	fakeClient := NewTestRestClient()
 	expected := fakeClient.Pods().Items
 	emulator := NewClientEmulator()
 
@@ -252,7 +143,7 @@ func TestSyncPods(t *testing.T) {
 
 func TestSyncServices(t *testing.T) {
 
-	fakeClient := NewRestClient()
+	fakeClient := NewTestRestClient()
 	expected := fakeClient.Services().Items
 	emulator := NewClientEmulator()
 
@@ -278,7 +169,7 @@ func TestSyncServices(t *testing.T) {
 
 func TestSyncReplicationControllers(t *testing.T) {
 
-	fakeClient := NewRestClient()
+	fakeClient := NewTestRestClient()
 	expected := fakeClient.ReplicationControllers().Items
 	emulator := NewClientEmulator()
 
