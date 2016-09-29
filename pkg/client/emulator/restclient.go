09b0975db1d79aca8c57dd181196d72ca7d64aad
Jan Chaloupka <jchaloup@redhat.com>
2016-09-29 23:17:09 +0200

    WIP: implement fake watch for io.ReadCloser byte stream

--- pkg/client/emulator/restclient.go
+++ pkg/client/emulator/restclient.go
@@ -13,6 +13,8 @@ import (
 	"k8s.io/kubernetes/pkg/api/testapi"
 	"strings"
 	"k8s.io/kubernetes/pkg/apis/extensions"
+	"io"
+	"k8s.io/kubernetes/pkg/watch"
 )
 
 // RESTClient provides a fake RESTClient interface.
@@ -27,6 +29,9 @@ type RESTClient struct {
 	servicesDataSource func() *api.ServiceList
 	replicationControllersDataSource func() *api.ReplicationControllerList
 	replicaSetsDataSource func() *extensions.ReplicaSetList
+
+	podsWatcherReadGetter *WatchBuffer
+	servicesWatcherReadGetter *WatchBuffer
 }
 
 func (c *RESTClient) Pods() *api.PodList {
@@ -44,6 +49,27 @@ func (c *RESTClient) ReplicationControllers() *api.ReplicationControllerList {
 func (c *RESTClient) ReplicaSets() *extensions.ReplicaSetList {
 	return c.replicaSetsDataSource()
 }
+
+
+func (c *RESTClient) EmitPodWatchEvent(eType watch.EventType, object *api.Pod) {
+	if c.podsWatcherReadGetter != nil {
+		//event := watch.Event{
+		//	Type: eType,
+		//	Object: object,
+		//}
+		var buffer bytes.Buffer
+		buffer.WriteString("{\"type\":\"")
+		buffer.WriteString(string(eType))
+		buffer.WriteString("\",\"object\":")
+
+		payload := []byte(buffer.String())
+		payload = append(payload, ([]byte)(runtime.EncodeOrDie(testapi.Default.Codec(), object))...)
+		payload = append(payload, []byte("}")...)
+
+		c.podsWatcherReadGetter.Write(payload)
+	}
+}
+
 
 func (c *RESTClient) Get() *restclient.Request {
 	return c.request("GET")
@@ -97,6 +123,43 @@ func splitPath(path string) []string {
         return strings.Split(path, "/")
 }
 
+func (c *RESTClient) createListReadCloser(resource string) (rc *io.ReadCloser, err error) {
+	var obj runtime.Object
+	switch resource {
+		case "pods":
+			obj = c.Pods()
+		case "services":
+			obj = c.Services()
+		case "replicationcontrollers":
+			obj = c.ReplicationControllers()
+		default:
+			return nil, fmt.Errorf("Resource %s not recognized", resource)
+	}
+
+	nopCloser := ioutil.NopCloser(bytes.NewReader([]byte(runtime.EncodeOrDie(testapi.Default.Codec(), obj))))
+	return &nopCloser, nil
+
+}
+
+func (c *RESTClient) createWatchReadCloser(resource string) (rc *WatchBuffer, err error) {
+	rc = NewWatchBuffer()
+	switch resource {
+		case "pods":
+			if c.podsWatcherReadGetter != nil {
+				c.podsWatcherReadGetter.Close()
+			}
+			c.podsWatcherReadGetter = rc
+		case "services":
+			if c.servicesWatcherReadGetter != nil {
+				c.servicesWatcherReadGetter.Close()
+			}
+			c.servicesWatcherReadGetter = rc
+		default:
+			return nil, fmt.Errorf("Resource %s not recognized", resource)
+	}
+	return rc, nil
+}
+
 func (c *RESTClient) Do(req *http.Request) (*http.Response, error) {
 	if c.Err != nil {
 		return nil, c.Err
@@ -109,23 +172,27 @@ func (c *RESTClient) Do(req *http.Request) (*http.Response, error) {
 		return nil, fmt.Errorf("Missing resource in REST client request url")
 	}
 
-	var obj runtime.Object
-
-	switch parts[0] {
-		case "pods":
-			obj = c.Pods()
-		case "services":
-			obj = c.Services()
-		case "replicationcontrollers":
-			obj = c.ReplicationControllers()
-		default:
-			return nil, fmt.Errorf("Resource %s not recognized", parts[0])
-	}
-
 	header := http.Header{}
 	header.Set("Content-Type", runtime.ContentTypeJSON)
-	body := ioutil.NopCloser(bytes.NewReader([]byte(runtime.EncodeOrDie(testapi.Default.Codec(), obj))))
-	c.Resp = &http.Response{StatusCode: 200, Header: header, Body: body}
+
+	if parts[0] == "watch" {
+		if len(parts) < 2 {
+			return nil, fmt.Errorf("Missing resource in REST client request url")
+		}
+		body, err := c.createWatchReadCloser(parts[1])
+		if err != nil {
+			return nil, fmt.Errorf("Unable to create watcher for %s\n", parts[1])
+		}
+		//var t io.ReadCloser = body
+		c.Resp = &http.Response{StatusCode: 200, Header: header, Body: (io.ReadCloser)(body)}
+
+	} else {
+		body, err := c.createListReadCloser(parts[0])
+		if err != nil {
+			return nil, fmt.Errorf("Unable to create lister for %s\n", parts[1])
+		}
+		c.Resp = &http.Response{StatusCode: 200, Header: header, Body: *body}
+	}
 
 	return c.Resp, nil
 }
---
+++ pkg/client/emulator/watch.go
@@ -0,0 +1,119 @@
+package emulator
+
+import (
+	"bytes"
+	"io"
+	"fmt"
+	"time"
+)
+
+// Every watcher expects infinite byte stream
+// E.g. http.Body can provide continuous byte stream,
+// each chunk sent asynchronously (invocation of Read is blocked until new chunk)
+
+// Implementation of io.ReadCloser
+type WatchBuffer struct {
+	buf   *bytes.Buffer
+        read  chan []byte
+        write chan []byte
+        retc  chan retc
+
+	closed bool
+}
+
+type retc struct {
+	n int
+	e error
+}
+
+var _ io.ReadCloser = &WatchBuffer{}
+
+// Read watch events as byte stream
+func (w *WatchBuffer) Read(p []byte) (n int, err error) {
+	w.read <- p
+	ret := <-w.retc
+	return ret.n, ret.e
+}
+
+// Close all channels
+func (w *WatchBuffer) Close() error {
+	if !w.closed {
+		close(w.read)
+		close(w.write)
+		close(w.retc)
+		w.closed = true
+	}
+	return nil
+}
+
+// Write
+func (w *WatchBuffer) Write(data []byte) (nr int, err error)  {
+	w.write <- data
+	return len(data), nil
+}
+
+func (w *WatchBuffer) loop() {
+	defer w.Close()
+
+	var dataIn, dataOut []byte
+
+	for {
+		select {
+			case dataIn = <-w.write:
+				_, err := w.buf.Write(dataIn)
+				if err != nil {
+					// TODO(jchaloup): add log message
+					fmt.Println("Write error")
+					break
+				}
+			case dataOut = <-w.read:
+				if w.buf.Len() == 0 {
+					dataIn = <-w.write
+					_, err := w.buf.Write(dataIn)
+					if err != nil {
+						// TODO(jchaloup): add log message
+						fmt.Println("Write error")
+						break
+					}
+				}
+				nr, err := w.buf.Read(dataOut)
+				w.retc <-retc{nr,err}
+		}
+	}
+}
+
+
+func NewWatchBuffer() *WatchBuffer {
+	wb := &WatchBuffer{
+		buf: bytes.NewBuffer(nil),
+		read: make(chan []byte),
+		write: make(chan []byte),
+		retc: make(chan retc),
+		closed: false,
+	}
+
+	go wb.loop()
+	return wb
+}
+
+func main() {
+	buffer := NewWatchBuffer()
+
+	go func() {
+		buffer.Write([]byte("Ahoj"))
+		time.Sleep(5*time.Second)
+		buffer.Write([]byte(" Svete"))
+		//time.Sleep(time.Second)
+	}()
+
+	data := []byte{0,0,0,0,0,0}
+	buffer.Read(data)
+	fmt.Printf("\tdata: %s\n", data)
+	buffer.Read(data)
+	fmt.Printf("\tdata: %s\n", data)
+
+	time.Sleep(10*time.Second)
+
+	buffer.Close()
+	fmt.Println("Ahoj")
+}
