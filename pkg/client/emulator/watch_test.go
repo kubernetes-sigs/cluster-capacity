d31a6f23aac83f2f7e8ec46816c8f31ac637811c
Jan Chaloupka <jchaloup@redhat.com>
2016-09-30 13:23:25 +0200

    test pod watching

--- pkg/client/emulator/watch_test.go
+++ pkg/client/emulator/watch_test.go
@@ -3,36 +3,26 @@ package emulator
 import (
 	"testing"
 	"k8s.io/kubernetes/pkg/client/cache"
-	"fmt"
 	apitesting "k8s.io/kubernetes/pkg/api/testing"
 	"k8s.io/kubernetes/pkg/api/testapi"
 	"k8s.io/kubernetes/pkg/watch"
 	"time"
 	"k8s.io/kubernetes/pkg/fields"
 	"k8s.io/kubernetes/pkg/api"
-
+	"reflect"
 )
 
 func newTestRestClient() *RESTClient {
-
-	client := &RESTClient{
+	return &RESTClient{
 		NegotiatedSerializer: testapi.Default.NegotiatedSerializer(),
 	}
-
-	return client
 }
 
-// TODO:
-// - add one event, collect one event, test for equality, repeat
-// - add two events to buffer and then test both events are decoded one by one
-// - add one event, collect one event, close the watcher and check correct error is returned
-
-func TestWatchPods(t *testing.T) {
-	client := newTestRestClient()
+func getResourceWatcher(client cache.Getter, resource string) watch.Interface {
 	// client listerWatcher
-	listerWatcher := cache.NewListWatchFromClient(client, "pods", api.NamespaceAll, fields.ParseSelectorOrDie(""))
+	listerWatcher := cache.NewListWatchFromClient(client, resource, api.NamespaceAll, fields.ParseSelectorOrDie(""))
 	// ask for watcher data
-	timemoutseconds := int64(1)
+	timemoutseconds := int64(10)
 
 	options := api.ListOptions{
 		ResourceVersion: "0",
@@ -42,34 +32,83 @@ func TestWatchPods(t *testing.T) {
 	}
 
 	w, _ := listerWatcher.Watch(options)
+	return w
+}
 
-	item := &api.Pod{
+type eventTest struct{
+	event watch.EventType
+	item *api.Pod
+}
+
+func testWatch(tests []eventTest, resource string, t *testing.T) {
+
+	client := newTestRestClient()
+	w := getResourceWatcher(client, resource)
+
+	t.Logf("Emitting first two events")
+	client.EmitPodWatchEvent(tests[0].event, tests[0].item)
+	client.EmitPodWatchEvent(tests[1].event, tests[1].item)
+	// wait for a while so both events are in one byte stream
+	time.Sleep(time.Second)
+	sync := make(chan struct{})
+
+	// retrieve all events one by one in the same order
+	go func() {
+		for _, test := range tests {
+			t.Logf("Waiting for event")
+			event, ok := <-w.ResultChan()
+			if !ok {
+				t.Errorf("Unexpected watch close")
+			}
+			t.Logf("Event received")
+			if event.Type != test.event {
+				t.Errorf("Expected event type %q, got %q", test.event, event.Type)
+			}
+			if !reflect.DeepEqual(test.item, event.Object) {
+				t.Errorf("unexpected object: expected: %#v\n actual: %#v", test.item, event.Object)
+			}
+
+		}
+		sync<- struct{}{}
+	}()
+
+	// send remaining events
+	t.Logf("Emitting remaining events")
+	for _, test := range tests[2:] {
+		time.Sleep(time.Second)
+		client.EmitPodWatchEvent(test.event, test.item)
+		t.Logf("Event emitted")
+	}
+
+	// wait for all events
+	<-sync
+}
+
+func TestWatchPods(t *testing.T) {
+
+	pod := &api.Pod{
 		ObjectMeta: api.ObjectMeta{Name: "pod1", Namespace: "test", ResourceVersion: "10"},
 		Spec:       apitesting.DeepEqualSafePodSpec(),
 	}
 
-	fmt.Println("Emitting two events")
-	client.EmitPodWatchEvent(watch.Modified, item)
-	client.EmitPodWatchEvent(watch.Added, item)
-	time.Sleep(time.Second)
+	tests := []eventTest{
+		{
+			event: watch.Modified,
+			item: pod,
+		},
+		{
+			event: watch.Added,
+			item: pod,
+		},
+		{
+			event: watch.Modified,
+			item: pod,
+		},
+		{
+			event: watch.Deleted,
+			item: pod,
+		},
+	}
 
-	go func() {
-		//select {
-		//	case event, ok := <-w.ResultChan():
-		//		fmt.Printf("event type: %s", event.Type)
-		//}
-		for i:=0; i < 6; i++ {
-			fmt.Println("Waiting for event")
-			event := <-w.ResultChan()
-			fmt.Printf("event type: %s\n", event.Type)
-		}
-		fmt.Println("Watch ended")
-	}()
-
-	// send watch data
-	time.Sleep(2*time.Second)
-	client.EmitPodWatchEvent(watch.Modified, item)
-	time.Sleep(time.Second)
-	client.EmitPodWatchEvent(watch.Added, item)
-	time.Sleep(time.Second)
+	testWatch(tests, "pods", t)
 }
