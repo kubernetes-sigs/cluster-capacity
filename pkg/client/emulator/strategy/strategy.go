c81665a2d3d0546dca17b8c9adb4fe4516274df3
Jan Chaloupka <jchaloup@redhat.com>
2016-10-02 22:06:46 +0200

    implement predictive strategy's add pod (WIP)

--- pkg/client/emulator/strategy/strategy.go
+++ pkg/client/emulator/strategy/strategy.go
@@ -2,27 +2,38 @@ package strategy
 
 import (
 	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/meta"
 	"github.com/ingvagabund/cluster-capacity/pkg/client/emulator/store"
 	"fmt"
 )
 
 type Strategy interface {
 	// Add new objects
-	Add(objs []interface{}) error
+	Add(obj interface{}) error
 
 	// Update objects
-	Update(objs []interface{}) error
+	Update(obj interface{}) error
 
 	// Delete objects
-	Delete(objs []interface{}) error
+	Delete(obj interface{}) error
 }
 
 type predictiveStrategy struct {
 	resourceStore store.ResourceStore
 }
 
-func (*predictiveStrategy) addPod(pod *api.Pod) error {
+func (s *predictiveStrategy) addPod(pod *api.Pod) error {
 	// 1. get node the pod is scheduled on
+	node := &api.Node{
+		ObjectMeta: api.ObjectMeta{Name: pod.Spec.NodeName},
+	}
+	scheduledNode, exists, err := s.resourceStore.Get("nodes", meta.Object(node))
+	if err != nil {
+		return fmt.Errorf("Unable to get node: %v", err)
+	}
+	if !exists {
+		return fmt.Errorf("Unable to find scheduled pod's node")
+	}
 
 	// 2. update the node info to include new pod's resources
 
@@ -31,15 +42,22 @@ func (*predictiveStrategy) addPod(pod *api.Pod) error {
 }
 
 // Simulate creation of new object (only pods currently supported)
-func (*predictiveStrategy) Add(obj []interface{}) error {
-	return nil
+// The method returns error on the first occurence of processing error.
+// If so, all succesfully processed objects up to the first failed are reflected in the resource store.
+func (s *predictiveStrategy) Add(obj interface{}) error {
+	switch item := obj.(type) {
+		case *api.Pod:
+			return s.addPod(item)
+		default:
+			return fmt.Errorf("resource kind not recognized")
+	}
 }
 
-func (*predictiveStrategy) Update(objs []interface{}) error {
+func (s *predictiveStrategy) Update(obj interface{}) error {
 	return fmt.Errorf("Not implemented yet")
 }
 
-func (*predictiveStrategy) Delete(objs []interface{}) error {
+func (s *predictiveStrategy) Delete(obj interface{}) error {
 	return fmt.Errorf("Not implemented yet")
 }
 
---
+++ pkg/client/emulator/strategy/strategy_test.go
@@ -0,0 +1,125 @@
+package strategy
+
+import (
+	"fmt"
+	"testing"
+	goruntime "runtime"
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/version"
+	"k8s.io/kubernetes/pkg/api/resource"
+	apitesting "k8s.io/kubernetes/pkg/api/testing"
+	"github.com/ingvagabund/cluster-capacity/pkg/client/emulator/store"
+)
+
+func getTestNode(nodeName string) *api.Node {
+	return &api.Node{
+		ObjectMeta: api.ObjectMeta{Name: nodeName},
+		Spec:       api.NodeSpec{},
+		Status: api.NodeStatus{
+			Conditions: []api.NodeCondition{
+				{
+					Type:               api.NodeOutOfDisk,
+					Status:             api.ConditionFalse,
+					Reason:             "KubeletHasSufficientDisk",
+					Message:            fmt.Sprintf("kubelet has sufficient disk space available"),
+					LastHeartbeatTime:  unversioned.Time{},
+					LastTransitionTime: unversioned.Time{},
+				},
+				{
+					Type:               api.NodeMemoryPressure,
+					Status:             api.ConditionFalse,
+					Reason:             "KubeletHasSufficientMemory",
+					Message:            fmt.Sprintf("kubelet has sufficient memory available"),
+					LastHeartbeatTime:  unversioned.Time{},
+					LastTransitionTime: unversioned.Time{},
+				},
+				{
+					Type:               api.NodeDiskPressure,
+					Status:             api.ConditionFalse,
+					Reason:             "KubeletHasNoDiskPressure",
+					Message:            fmt.Sprintf("kubelet has no disk pressure"),
+					LastHeartbeatTime:  unversioned.Time{},
+					LastTransitionTime: unversioned.Time{},
+				},
+				{
+					Type:               api.NodeReady,
+					Status:             api.ConditionTrue,
+					Reason:             "KubeletReady",
+					Message:            fmt.Sprintf("kubelet is posting ready status"),
+					LastHeartbeatTime:  unversioned.Time{},
+					LastTransitionTime: unversioned.Time{},
+				},
+			},
+			NodeInfo: api.NodeSystemInfo{
+				MachineID:               "123",
+				SystemUUID:              "abc",
+				BootID:                  "1b3",
+				KernelVersion:           "3.16.0-0.bpo.4-amd64",
+				OSImage:                 "Debian GNU/Linux 7 (wheezy)",
+				OperatingSystem:         goruntime.GOOS,
+				Architecture:            goruntime.GOARCH,
+				ContainerRuntimeVersion: "test://1.5.0",
+				KubeletVersion:          version.Get().String(),
+				KubeProxyVersion:        version.Get().String(),
+			},
+			Capacity: api.ResourceList{
+				api.ResourceCPU:       *resource.NewMilliQuantity(2000, resource.DecimalSI),
+				api.ResourceMemory:    *resource.NewQuantity(10E9, resource.BinarySI),
+				api.ResourcePods:      *resource.NewQuantity(0, resource.DecimalSI),
+				api.ResourceNvidiaGPU: *resource.NewQuantity(0, resource.DecimalSI),
+			},
+			Allocatable: api.ResourceList{
+				api.ResourceCPU:       *resource.NewMilliQuantity(0, resource.DecimalSI),
+				api.ResourceMemory:    *resource.NewQuantity(0, resource.BinarySI),
+				api.ResourcePods:      *resource.NewQuantity(0, resource.DecimalSI),
+				api.ResourceNvidiaGPU: *resource.NewQuantity(0, resource.DecimalSI),
+			},
+			Addresses: []api.NodeAddress{
+				{Type: api.NodeLegacyHostIP, Address: "127.0.0.1"},
+				{Type: api.NodeInternalIP, Address: "127.0.0.1"},
+			},
+			Images: []api.ContainerImage{},
+		},
+	}
+}
+
+var testStrategyNode string = "node1"
+
+func newScheduledPod() *api.Pod {
+	pod := &api.Pod{
+		ObjectMeta: api.ObjectMeta{Name: "schedulerPod", Namespace: "test", ResourceVersion: "10"},
+		Spec:       apitesting.DeepEqualSafePodSpec(),
+	}
+
+	// schedule the pod on the node
+	pod.Spec.NodeName = testStrategyNode
+
+	return pod
+}
+
+func newTestStrategyResourceStore() store.ResourceStore {
+	return &store.FakeResourceStore{
+		NodesData: func() []api.Node {
+			return []api.Node{
+				*getTestNode(testStrategyNode),
+			}
+		},
+	}
+}
+
+
+func TestAddPodStrategy(t *testing.T) {
+	// 1. create resource storage and fill it with a fake node
+	resourceStorage := newTestStrategyResourceStore()
+	predictiveStrategy := NewPredictiveStrategy(resourceStorage)
+
+	// 2. create fake pod with some consumed resources assigned to the fake fake
+	scheduledPod := newScheduledPod()
+
+	// 3. run the strategy to retrieve the node from the resource store recomputing the node's allocatable
+	err := predictiveStrategy.Add(scheduledPod)
+	fmt.Println(err)
+
+	// 4. check both the update node and the pod is stored back into the resource store
+}
