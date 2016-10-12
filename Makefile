3bf58f08ee1895f5fc3d93c71cfffd5ee899907f
Jan Chaloupka <jchaloup@redhat.com>
2016-10-12 17:28:37 +0200

    add Makefile, testing pod and update test.sh

---
+++ Makefile
@@ -0,0 +1,4 @@
+build:
+	go build -o cluster-capacity github.com/ingvagabund/cluster-capacity
+run:
+	@./cluster-capacity --kubeconfig ~/.kube/config --master http://localhost:8080 --podspec=pod.yaml --verbose
--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-#!/bin/sh -x
+#!/bin/sh
 
-go test -v github.com/ingvagabund/cluster-capacity/pkg/client/emulator/strategy github.com/ingvagabund/cluster-capacity/pkg/client/emulator/restclient
+find . -iname "*_test.go" | grep -v "./vendor" | xargs dirname | sort -u | xargs echo $(sed -s "s/\./github.com\/ingvagabund\/cluster-capacity/") | xargs go test -v
