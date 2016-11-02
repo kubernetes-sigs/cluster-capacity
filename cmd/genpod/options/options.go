52b6c38b37cd9a34777f9023803c956adee65df5
Jan Chaloupka <jchaloup@redhat.com>
2016-11-02 15:32:01 +0100

    genpod: provide output format (yaml or json, yaml by default)

--- cmd/genpod/options/options.go
+++ cmd/genpod/options/options.go
@@ -9,6 +9,7 @@ type GenPodOptions struct {
 	Kubeconfig string
 	Verbose    bool
 	Namespace  string
+	Format     string
 }
 
 func NewGenPodOptions() *GenPodOptions {
@@ -20,4 +21,5 @@ func (s *GenPodOptions) AddFlags(fs *pflag.FlagSet) {
 	fs.StringVar(&s.Kubeconfig, "kubeconfig", s.Kubeconfig, "Path to kubeconfig file with authorization and master location information.")
 	fs.BoolVar(&s.Verbose, "verbose", s.Verbose, "Verbose mode")
 	fs.StringVar(&s.Namespace, "namespace", s.Namespace, "Cluster namespace")
+	fs.StringVar(&s.Format, "output", s.Format, "Output format")
 }
--- cmd/genpod/server.go
+++ cmd/genpod/server.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+
+	"github.com/ghodss/yaml"
 
 	"github.com/ingvagabund/cluster-capacity/cmd/genpod/options"
 	nspod "github.com/ingvagabund/cluster-capacity/pkg/client"
@@ -14,7 +18,7 @@ import (
 func NewGenPodCommand() *cobra.Command {
 	opt := options.NewGenPodOptions()
 	cmd := &cobra.Command{
-		Use:   "genpod --master MASTER --kubeconfig KUBECONFIG",
+		Use:   "genpod --master MASTER --kubeconfig KUBECONFIG --namespace NAMESPACE",
 		Short: "Generate pod based on namespace resource limits and node selector annotations",
 		Long:  "Generate pod based on namespace resource limits and node selector annotations",
 		Run: func(cmd *cobra.Command, args []string) {
@@ -46,6 +50,11 @@ func Validate(opt *options.GenPodOptions) error {
 	if len(opt.Namespace) == 0 {
 		return fmt.Errorf("Cluster namespace missing")
 	}
+
+	if len(opt.Format) > 0 && opt.Format != "json" && opt.Format != "yaml" {
+		return fmt.Errorf("Output format %v not recognized: only json and yaml are allowed", opt.Format)
+	}
+
 	return nil
 }
 
@@ -57,9 +66,23 @@ func Run(opt *options.GenPodOptions) error {
 
 	pod, err := nspod.RetrieveNamespacePod(client, opt.Namespace)
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
+		log.Fatalf("Error: %v\n", err)
 	} else {
-		fmt.Printf("Pod: %v\n", *pod)
+		//fmt.Printf("Pod: %v\n", *pod)
+		var stream []byte
+		var err error
+		switch opt.Format {
+		case "json":
+			stream, err = json.Marshal(*pod)
+		case "yaml":
+		default:
+			stream, err = yaml.Marshal(*pod)
+		}
+
+		if err != nil {
+			return fmt.Errorf("Failed to create pod: %v", err)
+		}
+		fmt.Print(string(stream))
 	}
 
 	return nil
