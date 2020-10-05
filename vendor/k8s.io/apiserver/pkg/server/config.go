3e87271fc0fc050605e21daabcb5978e74f9dbc4
Jan Chaloupka <jchaloup@redhat.com>
2020-10-05 14:01:02 +0200

    Bump k8s to 1.19.2

--- vendor/k8s.io/apiserver/pkg/server/config.go
+++ vendor/k8s.io/apiserver/pkg/server/config.go
@@ -614,11 +614,11 @@ func (c completedConfig) New(name string, delegationTarget DelegationTarget) (*G
 			if err != nil {
 				return nil, err
 			}
-			// TODO: Once we get rid of /healthz consider changing this to post-start-hook.
-			err = s.addReadyzChecks(healthz.NewInformerSyncHealthz(c.SharedInformerFactory))
-			if err != nil {
-				return nil, err
-			}
+		}
+		// TODO: Once we get rid of /healthz consider changing this to post-start-hook.
+		err := s.addReadyzChecks(healthz.NewInformerSyncHealthz(c.SharedInformerFactory))
+		if err != nil {
+			return nil, err
 		}
 	}
 
@@ -626,6 +626,7 @@ func (c completedConfig) New(name string, delegationTarget DelegationTarget) (*G
 	if s.isPostStartHookRegistered(priorityAndFairnessConfigConsumerHookName) {
 	} else if c.FlowControl != nil {
 		err := s.AddPostStartHook(priorityAndFairnessConfigConsumerHookName, func(context PostStartHookContext) error {
+			go c.FlowControl.MaintainObservations(context.StopCh)
 			go c.FlowControl.Run(context.StopCh)
 			return nil
 		})
