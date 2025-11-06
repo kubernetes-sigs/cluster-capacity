c3e30295623ac59b980c65d4d9fb80b33ef81f15
Vincent de Phily <vdephily@redhat.com>
2025-11-06 16:33:40 +0000

    chore: Bump k8s to 1.34.1
    Needed code change:
    * Types moved to different package
    * BindPlugin interface changed signature

--- test/run-e2e-tests.sh
+++ test/run-e2e-tests.sh
@@ -26,9 +26,9 @@ KIND_E2E=${KIND_E2E:-}
 if [ -n "$KIND_E2E" ]; then
     # If we did not set SKIP_INSTALL
     if [ -z "$SKIP_INSTALL" ]; then
-        K8S_VERSION=${KUBERNETES_VERSION:-v1.33.0}
+        K8S_VERSION=${KUBERNETES_VERSION:-v1.34.1}
         curl -Lo kubectl https://dl.k8s.io/release/${K8S_VERSION}/bin/linux/amd64/kubectl && chmod +x kubectl && mv kubectl /usr/local/bin/
-        wget https://github.com/kubernetes-sigs/kind/releases/download/v0.27.0/kind-linux-amd64
+        wget https://github.com/kubernetes-sigs/kind/releases/download/v0.30.0/kind-linux-amd64
         chmod +x kind-linux-amd64
         mv kind-linux-amd64 kind
         export PATH=$PATH:$PWD
